a777690326aecffaf33631104ba6241b546ad3fb
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2023-08-07 01:03:32 +0000

    Add quotes API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import { useState, useEffect } from "react";
 function App() {
 
-  const [quote, setQuote] = useState([]);
-	const [author, setAuthor] = useState("");
+  const [quotes, setQuotes] = useState([]);
 
-	useEffect(() => {
-    const category = 'happiness';
-    const apiKey = process.env.API_KEY;
-
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-          headers: { 'X-Api-Key': apiKey },
+        const response = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
+          headers: { 'X-Api-Key': 'g0pznL+NR1xHhrn1FKVhAg==qIfV0zRQdgTtQU4t'},
         });
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        setQuote(data);
-        setAuthor(data);
-        console.log(data)
+        setQuotes(data);
       } catch (error) {
         console.error('Error: ', error.message);
       }
@@ -32,10 +26,11 @@ function App() {
     <div className="bg-stone-500 h-screen p-2">
       <header className='text-stone-50 text-center'>
         <h1 className='text-4xl'>Thoughts in Transit</h1>
-        <p>{quote}-{author}</p>
+        <blockquote>{quotes.quote}</blockquote>
+        <p>- {quotes.author}</p>
       </header>
     
-      <body className="text-stone-50">
+      <section className="text-stone-50">
         <p>Date: 08-03-23</p>
         <p>*option to choose random question of the day via API*</p>
         <p>*option to select froma list a topic, then randomize*</p>
@@ -46,8 +41,8 @@ function App() {
         <textarea id="message" rows="4" className="block m-2 p-2 w-5/6 h-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="I am grateful for..."></textarea>
         <p>More about my day...</p>
         <textarea id="message" rows="4" className="block m-2 p-2 w-5/6 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Today I went to the beach and saw..."></textarea>
-        <button type="button" class="text-white bg-stone-400 dark:bg-stone-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save Journal Entry</button>
-      </body>
+        <button type="button" className="text-white bg-stone-400 dark:bg-stone-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save Journal Entry</button>
+      </section>
     </div>
   );
 }
